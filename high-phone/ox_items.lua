d4b483084c960ce4b2e90165ca839e635c9a195e
Ted <35614452+Tadehz@users.noreply.github.com>
2022-12-03 18:25:53 +0200

    Update and rename ox_inventory.lua to ox_items.lua

---
+++ high-phone/ox_items.lua
@@ -0,0 +1,51 @@
+-- Items for ox_inventory, copy and paste this below the return line inside the 'ox_inventory/data/items.lua' file.
+['phone'] = {
+	label = "Phone",
+	weight = 164,
+	stack = false,
+	client = {
+		export = "high_phone.openPhone",
+		remove = function(total)
+			local isOpen = exports.high_phone:isOpen()
+			if total < 1 and not isOpen then
+				exports.high_phone:closePhone()
+			end
+		end,
+	} 
+},
+
+['blue_phone'] = {
+	label = "Phone Blue",
+	weight = 164,
+	stack = false,
+},
+
+['red_phone'] = {
+	label = "Phone Red",
+	weight = 164,
+	stack = false,
+},
+
+['green_phone'] = {
+	label = "Phone Green",
+	weight = 164,
+	stack = false,
+},
+
+['gold_phone'] = {
+	label = "Phone Gold",
+	weight = 164,
+	stack = false,
+},
+
+['purple_phone'] = {
+	label = "Phone Purple",
+	weight = 164,
+	stack = false,
+},
+
+['black_phone'] = {
+	label = "Phone Black",
+	weight = 164,
+	stack = false,
+}
